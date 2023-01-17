6fba970838b2cd8aaafeb96ebda5044ac35f122a
“ralphcnunez” <ralphcnunez@gmail.com>
2023-01-17 11:28:52 -0500

    1st verison of todo list

---
+++ src/main/java/com/example/springboottodoapplication/controllers/TodoFormController.java
@@ -0,0 +1,48 @@
+package com.example.springboottodoapplication.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.example.springboottodoapplication.models.TodoItem;
+import com.example.springboottodoapplication.repositories.TodoItemRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class TodoFormController {
+    
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+
+    @GetMapping("/create-todo")
+    public String showCreateForm(TodoItem todoItem){
+        return "add-todo-item";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        TodoItem todoItem = todoItemRepository
+        .findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("TodoItem id: " + id + " not found"));
+    
+        model.addAttribute("todo", todoItem);
+        return "update-todo-item";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTodoItem(@PathVariable("id") long id, Model model) {
+        TodoItem todoItem = todoItemRepository
+        .findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("TodoItem id: " + id + " not found"));
+    
+        todoItemRepository.delete(todoItem);
+        return "redirect:/";
+    }
+
+    
+}
